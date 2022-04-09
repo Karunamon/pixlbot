494f3499df6b934b11e85eab514ca7c69c126948
Mike Parks <mparks@tkware.info>
2022-04-09 10:48:16 -0600

    Make fclock UTC-aware

--- cogs/ftime.py
+++ cogs/ftime.py
@@ -1,5 +1,5 @@
 import re
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from math import floor
 
 import discord
@@ -9,7 +9,7 @@ from discord.ext import commands
 
 from util import guilds
 
-zerotime = datetime(1984, 1, 1, 0, 0, 0, 0)
+zerotime = datetime(1984, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
 zerof = 6011000000000
 
 
@@ -23,15 +23,15 @@ class FelysianClock(commands.Cog):
     @fclock.command(name='now', guild_ids=guilds, description="Return current Felysian time")
     async def now(self, ctx: ApplicationContext):
         await ctx.defer()
-        timediff = datetime.now() - zerotime
+        timediff = datetime.now(timezone.utc) - zerotime
         secs = floor(zerof + timediff.total_seconds())
         strsecs = str(secs)
         await ctx.respond(f"`{strsecs[0:4]}:{strsecs[4:7]}.{strsecs[7:10]}.{strsecs[10:13]}`")
 
     @fclock.command(name='toearth', guild_ids=guilds, description="Return the Earth time from a given Felysian time")
     async def toearth(self, ctx: ApplicationContext,
-                      date: discord.Option(str, 'A felysian time like 6012:207.604.595')):
-        await ctx.defer(ephemeral=True)
+                      date: discord.Option(str, 'A Felysian time like 6012:207.604.595')):
+        await ctx.defer()
         m = re.match(r'^\d{13}$', date) or re.match(r'^\d{4}[:.]\d{3}.\d{3}.\d{3}$', date)
         if not m:
             await ctx.respond('Invalid date.')
@@ -39,7 +39,7 @@ class FelysianClock(commands.Cog):
         givenf = int(m.string.replace(':', '').replace('.', ''))
         tdf = zerof - givenf
         te = zerotime - timedelta(seconds=tdf)
-        await ctx.respond(te.strftime("%A %d-%b-%Y at %H:%M:%S"))
+        await ctx.respond(te.strftime(f"{date} is %A, %b %d %Y at %H:%M:%S"))
 
 
 def setup(bot):
